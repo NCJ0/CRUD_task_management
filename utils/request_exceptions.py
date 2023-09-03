0bd38df5a42d4c35ed709ceefc24a92c17027fd9
NCJ0 <nichotipong@gmail.com>
2023-09-03 13:23:24 +0700

    chore: remove unused import

--- utils/request_exceptions.py
+++ utils/request_exceptions.py
@@ -2,7 +2,6 @@ from fastapi.encoders import jsonable_encoder
 from fastapi.exceptions import RequestValidationError
 
 from starlette.exceptions import HTTPException
-from starlette.requests import Request
 from starlette.responses import JSONResponse
 from starlette.status import HTTP_422_UNPROCESSABLE_ENTITY
 
