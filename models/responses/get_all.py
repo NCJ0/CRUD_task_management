e0f5910c4b06e825afa751a73cf8b0b35a19e777
NCJ0 <nichotipong@gmail.com>
2023-09-03 23:49:57 +0700

    chore: use pydantic model as a response format for GET all tasks endpoint

---
+++ models/responses/get_all.py
@@ -0,0 +1,38 @@
+from pydantic import BaseModel, field_validator
+from datetime import datetime
+
+from constants.datetime import DateTimeConstant
+
+
+class GetAllItemResponse(BaseModel):
+    task_id: str
+    user_id: str
+    title: str
+    description: str
+    due_date: str
+    status: str
+    created_at: str
+    created_by: str
+    updated_at: str
+    updated_by: str
+
+
+    @field_validator('due_date', mode="before")
+    def string_due_date_to_datetime(cls, v: object) -> object:
+        if isinstance(v, datetime):
+            return datetime.strftime(v, DateTimeConstant.GLOBAL_TIME_FORMAT)
+        return v
+
+    @field_validator('created_at', mode="before")
+    def string_created_at_to_datetime(cls, v: object) -> object:
+        if isinstance(v, datetime):
+            return datetime.strftime(v, DateTimeConstant.GLOBAL_TIME_FORMAT)
+        return v
+
+    @field_validator('updated_at', mode="before")
+    def string_updated_at_to_datetime(cls, v: object) -> object:
+        if isinstance(v, datetime):
+            return datetime.strftime(v, DateTimeConstant.GLOBAL_TIME_FORMAT)
+        return v
+
+
--- views/task.py
+++ views/task.py
@@ -4,6 +4,7 @@ from fastapi import APIRouter, Depends
 
 from models.task import Task
 from models.responses.create_item import CreateItemResponse
+from models.responses.get_all import GetAllItemResponse
 from config.db import db
 
 from schemas.task import TaskCreateSchema
@@ -25,9 +26,21 @@ task_api = APIRouter(
 @task_api.get("/")
 async def get_all_tasks(db_session=Depends(db.get_db)):
     tasks = await Task.get_all(db_session)
+    tasks_list = [GetAllItemResponse(
+        task_id=task.task_id,
+        user_id=task.user_id,
+        title=task.title,
+        description=task.description,
+        due_date=task.due_date,
+        status=task.status,
+        created_at=task.created_at,
+        created_by=task.created_by,
+        updated_at=task.updated_at,
+        updated_by=task.updated_by
+    ).model_dump() for task in tasks]
     if not tasks:
-        return ServiceResult(AppException.GetAllTask())
-    result = ServiceResult(tasks)
+        return ServiceResult(AppException.GetAllTask(tasks))
+    result = ServiceResult(tasks_list)
     return handle_result(result)
 
 
